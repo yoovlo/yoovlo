671caf9b43dae54966935051b6121de68da9a634
Vagif Kuliyev <admin@yoovlo.org>
2022-03-26 20:39:19 +0000

    removed app_id comments

--- src/main/java/org/yoovlo/shr/java/req/Req.java
+++ src/main/java/org/yoovlo/shr/java/req/Req.java
@@ -25,14 +25,6 @@ public class Req {
 	/* action */
 	public String a;
 	/*
-	app_id: this is identifying the entity that effectively models what has been implemented in various software as tenant,domain,workspace,app
-		the most generic implementation of tenant is: an App in a parent (or root) Meta App
-		what we traditionally think of as Tenant, is essentially App defined entirely at runtime in data: without any custom,compiled functionality
-			whereas what we think of traditionally as an App is a tenant App in a Meta App, with the ability to execute custom code 
-				compiled,scripted,defined in external services,etc
-			they are however the same construct, with an App being the super Type by nature of having the ability to implement custom code
-				a traditional tenant often lacks compiled functionality, entirely defined by runtime capabilities
-				while an App is compiled and is a tenant of some data and compute structure
 	*/
 	public int app_id;
 	/* body */
